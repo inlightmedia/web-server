f5577d9ee2970d6058293cd5efc84176c1380188
Joshua Dyck <josh.dyck@inlightmedia.ca>
2015-11-13 20:00:45 -0500

    updated language

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(middleware.requireAuthentication);
 
 // having middleware in the app.get requiers authenticaiotn for just one route not every route in the app
 app.get('/about', middleware.requireAuthentication, function (request, response) { //get tis the http request method also put patch delete
-	response.send('About Us');
+	response.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public')); 
